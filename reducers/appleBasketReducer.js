4c55bc30dc7dee947f435a706c9788e3155e4811
liuqiuchen <lqcdsns@qq.com>
2017-01-22 18:42:32 +0800

    commit

---
+++ reducers/appleBasketReducer.js
@@ -0,0 +1,14 @@
+// 苹果篮子的state结构
+/*
+{
+    isPicking: false,
+    newAppleId: 1,
+    apples: [
+        {
+            id: 0,
+            weight: 235,
+            isEaten: false
+        }
+    ]
+}
+*/
